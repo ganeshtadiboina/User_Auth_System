938eea874ce89c6e0322982c449c6a9723a3a46b
Ganesh Tadiboina <ganeshtadiboina3127@gmail.com>
2025-04-06 08:35:27 +0530

    Update CORS to allow Vercel frontend

--- server/server.js
+++ server/server.js
@@ -11,11 +11,12 @@ const app = express();
 const port = process.env.PORT || 4000
 
 connectDB();
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://user-auth-system-two.vercel.app'];
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin : allowedOrigins, credentials:true}))
+app.use(cors({origin : allowedOrigins,
+  credentials:true}))
 
 // Api Endpoints
 app.get('/' , (req , res) => {
