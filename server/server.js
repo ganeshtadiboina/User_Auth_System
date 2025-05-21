111810761c43ff4de7ca4f9a3441ebed0a387894
Ganesh Tadiboina <ganeshtadiboina3127@gmail.com>
2025-05-21 17:24:49 +0530

    network error 404 resolved

--- server/server.js
+++ server/server.js
@@ -9,30 +9,29 @@ import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
-
 connectDB();
+
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://user-auth-system-two.vercel.app'
+  'https://user-auth-system-frontend1.vercel.app'  
 ];
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin : allowedOrigins,
-  credentials:true}))
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 // Api Endpoints
-app.get('/' , (req , res) => {
+app.get('/', (req, res) => {
   res.send("API working");
-})
+});
 
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 
-
-if(process.env.NODE_ENV !== "production"){
-  const port = process.env.PORT || 4000
-  app.listen(port, () => console.log(`Server started on PORT:${port}`));
+let server;
+if (process.env.NODE_ENV !== "deployment") {
+  const port = process.env.PORT || 4000;
+  server = app.listen(port, () => console.log(`Server started on PORT: ${port}`));
 }
 
-export default server;
+export default app;
