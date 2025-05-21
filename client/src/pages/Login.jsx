ded06c16927b90f6ef55d7ea6d8f55425ffe0fed
Ganesh Tadiboina <ganeshtadiboina3127@gmail.com>
2025-05-21 17:14:20 +0530

    solving network error

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
 
         axios.defaults.withCredentials = true;
         if(state === 'Sign Up'){
-          const {data} = await axios.post(backendUrl + '/api/auth/register',{name,email,password})
+          const {data} = await axios.post(backendUrl + '/api/auth/register',{name,email,password}, { withCredentials: true})
 
           if(data.success){
             setIsLoggedin(true)
@@ -31,7 +31,7 @@ const Login = () => {
             toast.error(data.message);
           }
         }else{
-          const {data} = await axios.post(backendUrl + '/api/auth/login',{email,password})
+          const {data} = await axios.post(backendUrl + '/api/auth/login',{email,password}, { withCredentials: true});
 
           if(data.success){
             setIsLoggedin(true)
